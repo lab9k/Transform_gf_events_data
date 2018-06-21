3dbec5db8fb92057188292d57b2bc4a47cd36c6e
Ruben Alliet <allietruben@gmail.com>
2018-06-21 21:58:52 +0200

    building up event structure

--- transform_events_data.py
+++ transform_events_data.py
@@ -2,7 +2,7 @@ import json
 import requests
 
 # All filtered Gentse feesten events
-FilteredEvents = []
+filteredEvents = []
 
 # Load json data gentse feesten events
 with open('data/gentsefeestenevents.json') as json_data:
@@ -15,20 +15,44 @@ with open('data/gentsefeestenevents.json') as json_data:
         # API request to get the location for our event
         r = requests.get(event["location"])
         location = r.json()
-        # print(location["address"])
         
-        # rebuild data structure for event 
+        # build data structure for 1 event 
         filteredEvent = {}
+        # all data for each event
+
+        # the event doesn't have an address/name
+        if location["address"] == None or event["name"] == None:
+          continue
+        
         filteredEvent["name"] = event["name"]["nl"]
-        filteredEvent["address"] = location["address"]["streetAddress"]
+
+        if event["description"] != None: 
+          filteredEvent["description"] = event["description"]["nl"]
+
+        if event["image"] != None:
+          filteredEvent["image_url"] = event["image"]["thumbnailUrl"]     
+        
+        if event["startDate"] != None:
+          filteredEvent["startDate"] = event["startDate"]
+
+        if event["endDate"] != None:
+          filteredEvent["endDate"] = event["endDate"]  
 
         # list of filtered events
-        FilteredEvents.append(filteredEvent)
+        filteredEvents.append(filteredEvent)
+
+        print(count)
 
         # TESTING PURPOSES OTHERWISE ALL 5400 EVENTS WILL BE LOOPED
-        if count == 2:
+        if count == 20:
           break
 
 
-    print(count)
-    print(FilteredEvents)
+    print("amount of events with location:", count)
+    # print(filteredEvents)
+    
+    # save output as JSON file 
+    with open('data/filtered_gf_events.json', 'w') as outfile:
+      json.dump(filteredEvents, outfile)
+
+
